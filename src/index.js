e75948f0dd259274c6eaa5e793218e8733860dea
alex-vn <alex.vukovity@gmail.com>
2017-10-26 16:37:25 +0200

    DEVELOP: Promoting search bar from functional to class based component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyAHX0eFEtPcC9j8QgUybzaCGHj-gULxYnM'
 
-const App = ()=> {
-    return (<h1>hey</h1>)
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
